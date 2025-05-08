d1c04e53fdc1545b295a32befa624d5604d193cb
binghan60 <binghan60@gmail.com>
2025-05-08 14:33:18 +0800

    jj

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 // app.use(bodyParser.json());
 const config = {
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
