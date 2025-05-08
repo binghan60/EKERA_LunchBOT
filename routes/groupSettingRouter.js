46e273abed5f9315b7da649f8a23717d780bbe83
binghan60 <binghan60@gmail.com>
2025-05-08 11:17:05 +0800

    gg

--- routes/groupSettingRouter.js
+++ routes/groupSettingRouter.js
@@ -6,10 +6,10 @@ router.get('/', (req, res) => {
     res.send('Hello from group setting router!');
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     const groupId = req.params.id;
     console.log('Group ID:', groupId);
-    const groupSetting = GroupSetting.findOne({ groupId });
+    const groupSetting = await GroupSetting.findOne({ groupId });
     console.log(groupSetting);
     if (!groupSetting) {
         return res.status(404).send('Group setting not found');
