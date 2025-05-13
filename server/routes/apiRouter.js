49e5eea59f635b047ca3a77d0166f15874cac76f
binghan60 <binghan60@gmail.com>
2025-05-13 10:31:49 +0800

    RR

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const restaurantRoutes = require('./restaurantRouter');
 const groupSettingRoutes = require('./groupSettingRouter.js');
-
+const randomRestaurantRoutes = require('./randomRestaurantRouter.js');
 const router = express.Router();
 router.use(express.json());
 router.use('/group-settings', groupSettingRoutes);
 router.use('/restaurant', restaurantRoutes);
+router.use('/random-restaurant', randomRestaurantRoutes);
 
 module.exports = router;
