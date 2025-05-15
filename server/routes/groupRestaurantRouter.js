1f3b28550214c23fc1dfd63b190de69781652067
binghan60 <binghan60@gmail.com>
2025-05-15 11:00:59 +0800

    新增群組餐廳路由，實現根據 groupId 查詢群組餐廳的功能

---
+++ server/routes/groupRestaurantRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const Restaurant = require('../models/Restaurant');
+const GroupRestaurant = require('../models/GroupRestaurant');
+
+router.get('/:groupId', async (req, res) => {
+    try {
+        const groupId = req.params.groupId;
+        const restaurants = await GroupRestaurant.find({ groupId }).populate('restaurantId');
+        res.status(200).json(restaurants);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching group restaurants', error });
+    }
+});
+
+module.exports = router;
