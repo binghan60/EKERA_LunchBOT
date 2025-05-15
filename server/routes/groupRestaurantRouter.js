54ddc3a9936c6c45b28e5ae7991c64440f05c15e
binghan60 <binghan60@gmail.com>
2025-05-15 11:07:07 +0800

    更新群組餐廳路由，修正查詢邏輯以僅返回 restaurantId 欄位

--- server/routes/groupRestaurantRouter.js
+++ server/routes/groupRestaurantRouter.js
@@ -6,7 +6,7 @@ const GroupRestaurant = require('../models/GroupRestaurant');
 router.get('/:groupId', async (req, res) => {
     try {
         const groupId = req.params.groupId;
-        const restaurants = await GroupRestaurant.find({ groupId }).populate('restaurantId');
+        const restaurants = (await GroupRestaurant.find({ groupId }).populate('restaurantId')).select('restaurantId');
         res.status(200).json(restaurants);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching group restaurants', error });
