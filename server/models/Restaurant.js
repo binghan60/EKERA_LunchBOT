f5a9b7c5e0ac0169254bed3aba783f9e49022f9b
binghan <binghan60@gmail.com>
2025-05-27 01:05:43 +0800

    修正 Restaurant.js 中的 schema 定義，修正 require 為 required，並調整 menu 項目的預設值格式

--- server/models/Restaurant.js
+++ server/models/Restaurant.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const restaurantSchema = new mongoose.Schema(
   {
-    groupId: { type: String, require: true },
+    groupId: { type: String, required: true },
     name: { type: String, required: true },
-    menu: [{ type: String, default: '' }],
+    menu: { type: [String], default: [] },
     address: { type: String, default: '' },
     phone: { type: String, default: '' },
     tags: [{ type: String, default: [] }],
