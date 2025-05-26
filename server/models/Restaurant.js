dbb52551cbdcc0d917da2de52b6f5a7e48ffe42c
binghan <binghan60@gmail.com>
2025-05-26 21:50:05 +0800

    格式化 Restaurant.js 中的 schema 定義，提升可讀性

--- server/models/Restaurant.js
+++ server/models/Restaurant.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose');
 
 const restaurantSchema = new mongoose.Schema(
-    {
-        groupId: { type: String, require: true },
-        name: { type: String, required: true },
-        menu: [{ type: String, default: '' }],
-        address: { type: String, default: '' },
-        phone: { type: String, default: '' },
-        tags: [{ type: String, default: [] }],
-        isActive: { type: Boolean, default: true },
-    },
-    {
-        timestamps: true,
-    }
+  {
+    groupId: { type: String, require: true },
+    name: { type: String, required: true },
+    menu: [{ type: String, default: '' }],
+    address: { type: String, default: '' },
+    phone: { type: String, default: '' },
+    tags: [{ type: String, default: [] }],
+    isActive: { type: Boolean, default: true },
+  },
+  {
+    timestamps: true,
+  }
 );
-restaurantSchema.index({ name: 1, address: 1 }, { unique: true });
 module.exports = mongoose.model('Restaurant', restaurantSchema);
