c4ab99e7bb68903ba9ec72a2de676fa0eb15666b
binghan <binghan60@gmail.com>
2025-06-17 22:06:41 +0800

    新增app.get('/')

--- server/index.js
+++ server/index.js
@@ -9,28 +9,30 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
 
 const app = express();
+app.get('/', async (req, res) => {
+  res.status(200).json({ message: 'LINE BOT' });
+});
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(cors());
 const config = {
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-    channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
 mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log('資料庫連線成功');
-    })
-    .catch((err) => {
-        console.log('資料庫連線失敗', err);
-    });
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('資料庫連線成功');
+  })
+  .catch((err) => {
+    console.log('資料庫連線失敗', err);
+  });
 
 app.use('/api', apiRoutes);
 app.use('/webhook', line.middleware(config), webhookRoutes(config));
 
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`✅ LINE Bot server running at http://localhost:${port}`);
-    console.log(`Swagger docs: http://localhost:${port}/api-docs`);
+  console.log(`✅ LINE Bot server running at http://localhost:${port}`);
+  console.log(`Swagger docs: http://localhost:${port}/api-docs`);
 });
